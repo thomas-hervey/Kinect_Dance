608f91c3243212a59b38c62c64193731b28e2f7c
zhullih1 <zhullih1@umbc.edu>
2013-02-11 19:46:46 -0500

    test commit

---
+++ dmx/libdmx.h
@@ -0,0 +1,15 @@
+#ifndef LIBDMX_H
+#define LIBDMX_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+#include <process.h>
+#include <assert.h>
+#include <tchar.h>
+#include <time.h>
+#include <conio.h>
+#include "Ftd2xx.h"
+
+
+#endif
