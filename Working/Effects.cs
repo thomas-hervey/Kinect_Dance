b6b619b4394e81bdca619c1bf163aead623b6edc
zhullih1 <zhullih1@umbc.edu>
2013-04-10 20:45:59 -0400

    Added an effects utility class

---
+++ Working/Effects.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DmxComm;
+
+namespace DmxEffects
+{
+    class Effects
+    {
+        private static int tempc = 0;
+
+       
+        public static void exampleEffect1(DmxDriver device)
+        {
+            Console.WriteLine("DO TEST FUNCTION1");
+            tempc += 1;
+            device.setLampOn();
+            device.setDimmerLevel((byte)(tempc & 0xff));
+            device.setPan((byte)((tempc % 255) - 128));
+            device.setTilt((byte)((tempc % 255) - 128));
+            device.setColorContinuous(DmxDriver.color_t.PINK);
+        }
+
+        public static void exampleEffect2(DmxDriver device)
+        {
+            Console.WriteLine("DO TEST FUNCTION2");
+            tempc -= 1;
+            device.setLampOn();
+            device.setDimmerLevel((byte)(tempc & 0xff));
+            device.setPan((byte)((tempc % 255) - 128));
+            device.setTilt((byte)((tempc % 255) - 128));
+            device.setColorContinuous(DmxDriver.color_t.BLUE_101);
+            device.setGoboStandard(3);
+        }
+    }
+}
